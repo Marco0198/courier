a27c1d26f0a5e107a990193c853e372a20fc10b1
Marco Tshimanga <mtshimanga@oroafrica.co.za>
2022-05-03 12:08:59 +0200

    same changes

--- js/fastway_request.js
+++ js/fastway_request.js
@@ -1,16 +1,15 @@
-
 const credentials = JSON.stringify({
   email: "demo@collivery.co.za",
   password: "demo",
 });
 const header = {
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-    "X-App-Name": "My Custom App",
-    "X-App-Version": "0.2.1",
-    "X-App-Host": ".NET Framework 4.8",
-    "X-App-Lang": "C#",
-    "X-App-Url": "https://example.com",
+  "Content-Type": "application/json",
+  Accept: "application/json",
+  "X-App-Name": "My Custom App",
+  "X-App-Version": "0.2.1",
+  "X-App-Host": ".NET Framework 4.8",
+  "X-App-Lang": "C#",
+  "X-App-Url": "https://example.com",
 };
 const server = (val) => "https://api.collivery.co.za/v3/".concat(val);
 const parametrize = (url, params = {}) => {
@@ -21,11 +20,59 @@ const parametrize = (url, params = {}) => {
 };
 
 const api_key = "91d98ef3e489ee54f28ebfef8e6c6862";
-var form = document.forms["form"];
+
+let suburb ;
+
+function fetchAllSuburb() {
+  const url = new URL(
+    "https://api.collivery.co.za/v3/town_suburb_search?api_token=OpSjx5TlXGCGkzGAvUOm"
+  );
+
+  let params = {
+    search_text: document.form._collectionTownInput.value,
+  };
+  Object.keys(params).forEach((key) =>
+    url.searchParams.append(key, params[key])
+  );
+
+  let headers = {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    "X-App-Name": "My Custom App",
+    "X-App-Version": "0.2.1",
+    "X-App-Host": ".NET Framework 4.8",
+    "X-App-Lang": "C#",
+    "X-App-Url": "https://example.com",
+  }
+
+  fetch(url, {
+    method: "GET",
+    headers: headers,
+  })
+    .then((response) => response.json())
+    .then((res) => {
+     // townId = res.data[0].suburb.town.id;
+     //let townId= res.data[0].suburb.id;
+     //localStorage.setItem("townId", res.data[0].suburb.id);
+     console.log("ta reponse "+ res);
+     getTownId(res.data[0].suburb.town.id);
+      
+    });
+    
+}
+function getTownId(id){
+  
+  suburb=id;
+  console.log(suburb)
+  return suburb;
+}
+//fetchAllSuburb()
+
 form.onsubmit = async function (e) {
-    var townId=fetchAllSuburb();
-    console.log(townId),
-  e.preventDefault();
+ 
+ e.preventDefault();
+ console.log(suburb);
+ //console.log("this is"+city.townId)
   const url = `https://sa.api.fastway.org/v3/psc/lookup?api_key=${api_key}`;
   const token = "OpSjx5TlXGCGkzGAvUOm";
   const result = await Promise.allSettled([
@@ -70,7 +117,7 @@ form.onsubmit = async function (e) {
               quantity: 2,
             },
           ],
-          collection_town:townId,
+          collection_town:suburb,
           delivery_town: 200,
           collection_location_type: 1,
           delivery_location_type: 5,
@@ -79,13 +126,13 @@ form.onsubmit = async function (e) {
     )
       .then((response) => response.json())
       .then((res) => {
-        console.log(res.data);
+        console.log(res);
         appendDataCollivery(res.data);
       }),
   ]).catch((err) => {
     console.log(err);
   });
- // fetchAllSuburb()
+  // fetchAllSuburb()
 };
 function appendData(data) {
   var mainContainer = document.getElementById("myData");
@@ -234,113 +281,137 @@ const apiCall = {
     return await fetch(url, { method: "GET", headers: header });
   },
 };
-const actions =
-{
-    login:()=>
-    {
-        $(document).on("click","input:button",(ev)=>
-        {
-            let _fail = ()=>{ alert("Login Failed");};
-            let _success = (d)=>{$("meta[name='token']").attr("content",d.data.api_token); $(".onAuth").removeClass("d-none");};
-            if (ev.target.value === "login" ) 
-            {
-                if(!$("meta[name='token']").attr("content"))
-                {
-                    apiCall.login(JSON.stringify({ "email":$("#_email").val(), "password":$("#_password").val() }))
-                    .then(resp=> (resp.ok) ? resp.json() : "error")
-                    .then(d=> (d === "error") ? _fail() : _success(d));                       
-                }
-                else if($("meta[name='token']").attr("content"))
-                {
-                    $(".onAuth").removeClass("d-none");
-                }
-            };
-        });
-    }
-    ,collectionPoint:()=>
-    {
-        $(document).on("keyup","#_collectionTownInput",(ev)=>
-        {
-            apiCall.addressTownSuburbs($("#_collectionTownInput").val())
-                .then(resp=> resp.json())
-                .then(d=> 
-                {
-                   if (!d.data | $("#_collectionTownInput").val() < 4) return;
-                    $("#_collectionTown").html("");
-                   
-                    d.data.slice(0,10).map(k=>
-                    {
-                        $("#_collectionTown").append(`<option id="onme" >`.concat(k.formatted_result).concat("</option>"));
-                    });
-                });
-        });
-    }
-    ,deliveryPoint:()=>
-    {
-        $(document).on("keyup","#_deliveryTownInput",(ev)=>
-        {
-            apiCall.addressTownSuburbs($("#_deliveryTownInput").val())
-                .then(resp=> resp.json())
-                .then(d=> 
-                {
-                    if (!d.data | $("#_deliveryTownInput").val() < 4) return;
-                    $("#_deliveryTown").html("");
-                   
-                    d.data.slice(0,10).map(k=>
-                    {
-                        $("#_deliveryTown").append(`<option id="onme" >`.concat(k.formatted_result).concat("</option>"));
-                    });
-                });
-        });
-    }
-    ,resetQuote:()=>
-    {
-        $(document).on("click","input:button",(ev)=>
-        {
-            if (ev.target.value === "reset" ) 
-            {
-                $("#_collectionTownInput").val("");
-                $("#_deliveryTownInput").val("");
-                $("#deliveryType").prop("selectedIndex",0);
-                $(".qbox").html("");
-            }
-        });    
-    }
-    ,packageSize:()=>
-    {
+const actions = {
+  login: () => {
+    $(document).on("click", "input:button", (ev) => {
+      let _fail = () => {
+        alert("Login Failed");
+      };
+      let _success = (d) => {
+        $("meta[name='token']").attr("content", d.data.api_token);
+        $(".onAuth").removeClass("d-none");
+      };
+      if (ev.target.value === "login") {
+        if (!$("meta[name='token']").attr("content")) {
+          apiCall
+            .login(
+              JSON.stringify({
+                email: $("#_email").val(),
+                password: $("#_password").val(),
+              })
+            )
+            .then((resp) => (resp.ok ? resp.json() : "error"))
+            .then((d) => (d === "error" ? _fail() : _success(d)));
+        } else if ($("meta[name='token']").attr("content")) {
+          $(".onAuth").removeClass("d-none");
+        }
+      }
+    });
+  },
+  collectionPoint: () => {
+    $(document).on("keyup", "#_collectionTownInput", (ev) => {
+      apiCall
+        .addressTownSuburbs($("#_collectionTownInput").val())
+        .then((resp) => resp.json())
+        .then((d) => {
+          if (!d.data | ($("#_collectionTownInput").val() < 4)) return;
+          $("#_collectionTown").html("");
 
-    }
-    ,getQuote:()=>
-    {
-        $("#btn_name").click((ev) =>
-        {
-            if (ev.target.value === "quotes" ) 
-            {
-                //get collection town id
-                const _collectAddress = apiCall.addressTownSuburbs($("#_collectionTownInput").val().split(",")[0])
-                            .then(resp=> resp.json())
-                            .then(d=> 
-                            {
-                                log("COLLECTION: ",d.data[0].suburb.name + " - " + d.data[0].suburb.town.name);
-                                return { suburbId:d.data[0].suburb.id, townId:d.data[0].suburb.town.id};
-                            });
-                //get delivery town id
-                const _deliverAddress = apiCall.addressTownSuburbs($("#_deliveryTownInput").val().split(",")[0])
-                            .then(resp=> resp.json())
-                            .then(d=> 
-                            {
-                                log("DELIVER: ",d.data[0].suburb.name + " - " + d.data[0].suburb.town.name);
-                                return { suburbId:d.data[0].suburb.id, townId:d.data[0].suburb.town.id};
-                            });
-                //get quote
-                Promise.all([_collectAddress,_deliverAddress])
-                        .then(val=> 
-                        {
-                            //get parcel info
-                            var _parcel = parcelSizes($("#deliveryType").val());
-                            let quot =
-                            {
-                                /* optional/improvised parameters
+          d.data.slice(0, 10).map((k) => {
+            $("#_collectionTown").append(
+              `<option id="onme" >`
+                .concat(k.formatted_result)
+                .concat("</option>")
+            );
+          });
+        });
+    });
+  },
+  deliveryPoint: () => {
+    $(document).on("keyup", "#_deliveryTownInput", (ev) => {
+      apiCall
+        .addressTownSuburbs($("#_deliveryTownInput").val())
+        .then((resp) => resp.json())
+        .then((d) => {
+          if (!d.data | ($("#_deliveryTownInput").val() < 4)) return;
+          $("#_deliveryTown").html("");
+
+          d.data.slice(0, 10).map((k) => {
+            $("#_deliveryTown").append(
+              `<option id="onme" >`
+                .concat(k.formatted_result)
+                .concat("</option>")
+            );
+          });
+        });
+    });
+  },
+  resetQuote: () => {
+    $(document).on("click", "input:button", (ev) => {
+      if (ev.target.value === "reset") {
+        $("#_collectionTownInput").val("");
+        $("#_deliveryTownInput").val("");
+        $("#deliveryType").prop("selectedIndex", 0);
+        $(".qbox").html("");
+      }
+    });
+  },
+  packageSize: () => {},
+  getQuote: () => {
+    $("#btn_name").click((ev) => {
+      if (ev.target.value === "quotes") {
+        //get collection town id
+        const _collectAddress = apiCall
+          .addressTownSuburbs($("#_collectionTownInput").val().split(",")[0])
+          .then((resp) => resp.json())
+          .then((d) => {
+            log(
+              "COLLECTION: ",
+              d.data[0].suburb.name + " - " + d.data[0].suburb.town.name
+            );
+            return {
+              suburbId: d.data[0].suburb.id,
+              townId: d.data[0].suburb.town.id,
+            };
+          });
+        //get delivery town id
+        const _deliverAddress = apiCall
+          .addressTownSuburbs($("#_deliveryTownInput").val().split(",")[0])
+          .then((resp) => resp.json())
+          .then((d) => {
+            log(
+              "DELIVER: ",
+              d.data[0].suburb.name + " - " + d.data[0].suburb.town.name
+            );
+            return {
+              suburbId: d.data[0].suburb.id,
+              townId: d.data[0].suburb.town.id,
+            };
+          });
+        //get quote
+        Promise.all([_collectAddress, _deliverAddress],  fetch(url, {
+          method: "POST",
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+        
+            "RFCode": "JnB",
+            "Suburb": "Rondebosch",
+            "DestPostcode": 7700,
+            "WeightInKg": 7
+          }),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            appendData(data.result.services);
+          })).then((val) => {
+          //get parcel info
+          var _parcel = parcelSizes($("#deliveryType").val());
+          let quot = {
+            /* optional/improvised parameters
                                 "services": [2],
                                 "parcels": [{"length": 21.5,"width": 10,"height": 5.5,"weight": 5.2,"quantity": 1}],
                                 "collection_town": 147,
@@ -348,67 +419,42 @@ const actions =
                                 "delivery_time": "2025-01-25T16:00:00" 
                                 "delivery_town": 200,
                                  */
-                                "parcels": [_parcel],
-                                "collection_town": val[0].townId,
-                                "delivery_town": val[1].townId,
-                                "collection_location_type": 1,
-                                "delivery_location_type": 5
-                            };
-                            apiCall.requestQuote(quot)
-                                    .then(res=> res.json())
-                                    .then(d=> 
-                                    {
-                                        $(".qbox").html("");
-                                        d.data.map((val)=>
-                                        {
-                                            let parent = $("<div class='form-floating mb-3 _qval' />");
-                                            let total = $(`<input type='text' class='form-control' placeholder='header' id=${deliveryTypes(val.service_type)} value='R ${val.total}' />`);
-                                            let label = $(`<label class='form-label' for=${deliveryTypes(val.service_type)} > ${deliveryTypes(val.service_type)} : ${val.delivery_type}</label>`);
-                                            parent.append(total,label);
-                                            $(".qbox").append(parent);
-                                        });
-                                        //remove mb-3 to avoid margin overhang
-                                        $("._qval").last().removeClass("mb-3");
-                                    });
-                        });
-            }
-        });    
-    }
+            parcels: [_parcel],
+            collection_town: val[0].townId,
+            delivery_town: val[1].townId,
+            collection_location_type: 1,
+            delivery_location_type: 5,
+          };
+          apiCall
+            .requestQuote(quot)
+            .then((res) => res.json())
+            .then((d) => {
+              $(".qbox").html("");
+              d.data.map((val) => {
+                let parent = $("<div class='form-floating mb-3 _qval' />");
+                let total = $(
+                  `<input type='text' class='form-control' placeholder='header' id=${deliveryTypes(
+                    val.service_type
+                  )} value='R ${val.total}' />`
+                );
+                let label = $(
+                  `<label class='form-label' for=${deliveryTypes(
+                    val.service_type
+                  )} > ${deliveryTypes(val.service_type)} : ${
+                    val.delivery_type
+                  }</label>`
+                );
+                parent.append(total, label);
+                $(".qbox").append(parent);
+              });
+              //remove mb-3 to avoid margin overhang
+              $("._qval").last().removeClass("mb-3");
+            });
+        });
+      }
+    });
+  },
 };
 /* EXE */
 Object.values(actions).forEach((v) => v());
 
-function fetchAllSuburb(){
-
-    const url = new URL(
-        "https://api.collivery.co.za/v3/town_suburb_search?api_token=OpSjx5TlXGCGkzGAvUOm"
-    );
-    
-    let params = {
-        "search_text": document.form._collectionTownInput.value,
-    };
-    Object.keys(params)
-        .forEach(key => url.searchParams.append(key, params[key]));
-    
-    let headers = {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        "X-App-Name": "My Custom App",
-        "X-App-Version": "0.2.1",
-        "X-App-Host": ".NET Framework 4.8",
-        "X-App-Lang": "C#",
-        "X-App-Url": "https://example.com",
-    };
-    
-    fetch(url, {
-        method: "GET",
-        headers: headers,
-    })
-        .then(response => response.json())
-        .then((res) => {
-            townId=res.data[0].suburb.town.id
-            
-       console.log(res.data[0].suburb.town.id) 
-        });
-}
-//console.log(townId) 
